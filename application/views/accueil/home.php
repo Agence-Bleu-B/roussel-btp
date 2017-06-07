26188c90af6631c1e3751602001708aaa80650be
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-06-07 17:55:57 +0200

    modif bandeau

---
+++ application/views/accueil/home.php
@@ -0,0 +1,3 @@
+<div class="row">
+	<img class="home-img img-responsive" src="<?php echo img_url('bgTop.jpg'); ?>" alt="home image">
+</div>
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -21,7 +21,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
              
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-            </button> <a class="navbar-brand" href="#">Brand</a>
+            </button> <a class="navbar-brand" href="#">  </a>
           </div>
           
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
