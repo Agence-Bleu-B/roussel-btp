21cb65ccf7fe1aa551393352cd4e377bc1a0e431
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-06-09 11:55:02 +0200

    avancée design vendredi

--- application/views/accueil/home.php
+++ application/views/accueil/home.php
@@ -1,3 +1,8 @@
 <div class="row">
-	<img class="home-img img-responsive" src="<?php echo img_url('bgTop.jpg'); ?>" alt="home image">
+	<div class="bloc-accueil">
+		<img class="home-img img-responsive" src="<?php echo img_url('bgTop.jpg'); ?>">
+		<div class="col-sm-12">
+		<span><img class="roussel-txt img-responsive" src="<?php echo img_url('ROUSSELlogo.jpg'); ?>"></span>
+		</div>
+	</div>
 </div>
