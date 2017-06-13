5b5c13bb15675fca63a84a8696cc91285691204d
gwenael-lesaunier <lesaunier.gwenael@gmail.com>
2017-06-13 17:30:04 +0200

    avancée design

--- application/views/accueil/home.php
+++ application/views/accueil/home.php
@@ -7,33 +7,114 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-8">
-		<div class="bloc-contenu">
-			<h1 class="">actualités</h1>
+	<div class="col-md-8 bloc-contenu">
+		<div class="">
+			<h1 class="arrow_box">actualités</h1>
 		</div>
+		<div class="row jumbotron"></div>
 	</div>
-	<div class="col-md-4">
-		<div class="bloc-contenu">
-			<h1 class="">découvrez l'entreprise</h1>
+	<div class="col-md-4 bloc-contenu">
+		<div class="">
+			<h1 class="arrow_box">découvrez l'entreprise</h1>
 		</div>
-	</div>
-</div>
-<div class="row">
-	<div class="col-md-8">
-		<div class="bloc-contenu">
-			<h1 class="">nos réalisations</h1>
-		</div>
-	</div>
-	<div class="col-md-4">
-		<div class="bloc-contenu">
-			<h1 class="">chiffres clés</h1>
+		<div class="row jumbotron">
+			<p>Duplexque isdem diebus acciderat malum, quod et Theophilum insontem atrox interceperat casus, et Serenianus dignus exsecratione cunctorum, innoxius, modo non reclamante publico vigore, discessit...</p>
+			<a href="#" class="btn btn-custom"><span class="glyphicon glyphicon-zoom-in"></span> en savoir plus</a>
 		</div>
 	</div>
 </div>
 <div class="row">
-	<div class="col-md-12">
-		<div class="bloc-contenu">
-			<h1 class="">nos clients</h1>
+	<div class="col-md-8 bloc-contenu">
+		<div class="">
+			<h1 class="arrow_box">nos réalisations</h1>
 		</div>
+		<div class="row jumbotron">
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/1/" alt="...">
+	            </div>
+	        </div>
+	      
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/2/" alt="...">
+	            </div>
+	        </div>
+
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/3/" alt="...">
+	            </div>
+	        </div>
+
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/4/" alt="...">
+	            </div>  
+	        </div> 
+
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/4/" alt="...">
+	            </div>  
+	        </div>
+
+	        <div class="col-xs-6 col-sm-4 col-md-3">            
+	            <div class="thumbnail">
+	                <div class="caption">
+	                    <h4>Thumbnail Headline</h4>
+	                    <p>short thumbnail description</p>
+	                    <p><a href="" class="label label-danger">Zoom</a>
+	                    <a href="" class="label label-default">Download</a></p>
+	                </div>
+	                <img src="http://lorempixel.com/400/300/sports/4/" alt="...">
+	            </div>  
+	        </div>
+	        <a href="#" class="btn btn-custom"><span class="glyphicon glyphicon-zoom-in"></span>plus de réalisations</a>
+	    </div>
+	</div>
+	<div class="col-md-4 bloc-contenu">
+		<div class="">
+			<h1 class="arrow_box">chiffres clés</h1>
+		</div>
+		<div class="row jumbotron"></div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12 bloc-contenu">
+		<div class="">
+			<h1 class="arrow_box">nos clients</h1>
+		</div>
+		<div class="row jumbotron"></div>
 	</div>
 </div>
